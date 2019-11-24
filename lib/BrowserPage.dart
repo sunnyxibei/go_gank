8249c9b6fadc9e524e2703becf7922e1e1381df2
Yuanbin <jiayuanbin@gmail.com>
2019-11-24 21:31:29 +0800

    Try the hot reload function of flutter

---
+++ lib/BrowserPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BrowserPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("BrowserPage"),
+        ),
+        body: Center(child: Text("浏览页面待实现啊")));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:go_gank/BrowserPage.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Go Gank',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        //首页路由
+        home: MyHomePage(title: 'gank.io'),
+      );
 }
 
+///title 可以理解为Java｜Kotlin中的构造方法的参数
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
@@ -46,34 +33,21 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  ///简单地说，setState方法的作用是，属性改变时，可以回调build方法。
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      // Center 一个布局控件，有唯一的一个子控件，且居中对齐
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
           // Column is also layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -98,6 +72,16 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            FlatButton(
+                onPressed: () {
+                  //导航到浏览页面
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) {
+                    return new BrowserPage();
+                  }));
+                },
+                textColor: Colors.deepOrangeAccent,
+                child: Text("Trigger Browser Page !"))
           ],
         ),
       ),
